501f440e180971a53d80761368bf8cf2d1b9d669
Suprajna Sharma <suprasubedi11@gmail.com>
2023-08-27 15:19:27 +0545

    function to gather input tasks in array

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import { Todo } from './model';
 
 //react.fc===functional component
 const App: React.FC = () => {
 
   const [todo, setTodo] = useState<string>("");
   // console.log(todo);
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      //to empty the field after setting the data
+      setTodo("");
+    }
+  }
+  console.log(todos)
 
   return (
     <div className="App">
       <span className="heading">Task Manager</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   );
 }
