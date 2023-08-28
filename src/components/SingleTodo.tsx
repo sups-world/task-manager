1dd29c7f635d675c814368a78fdc559369896141
Suprajna Sharma <suprasubedi11@gmail.com>
2023-08-28 11:50:13 +0545

    css for tasks with icons

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Todo } from '../model';
+
+import { AiOutlineEdit, AiOutlineDelete } from 'react-icons/ai'
+import { MdOutlineDone } from 'react-icons/md'
+import "./styles.css";
 // type has equals but interface does not
 type Props = {
     todo: Todo,
@@ -11,6 +15,19 @@ const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }: Props) => {
     return (
         <form className='todos__single'>
             <span className="todos__single--text"> {todo.todo}</span>
+            <div>
+                <span className="icon">
+                    <AiOutlineEdit />
+                </span>
+                <span className="icon">
+                    <AiOutlineDelete />
+                </span>
+                <span className="icon">
+                    <MdOutlineDone />
+                </span>
+
+
+            </div>
         </form>
     )
 }
